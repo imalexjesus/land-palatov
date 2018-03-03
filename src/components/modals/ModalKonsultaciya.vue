5f953178bf47497e4e30a927b1a745aa3cd5cfc1
alexj <toalexjesus@gmail.com>
2018-03-03 03:53:33 +0200

    dsfs
    Signed-off-by: alexj <toalexjesus@gmail.com>

--- src/components/modals/ModalKonsultaciya.vue
+++ src/components/modals/ModalKonsultaciya.vue
@@ -3,7 +3,7 @@
 		<div class="uk-modal-dialog uk-margin-auto-vertical">
 			<button class="uk-modal-close-default" type="button" uk-close></button>
 
-			<form class="c-modal__form uk-form-stacked">
+			<form class="c-modal__form uk-form-stacked" @submit.prevent="submitForm">
 				<h2 class="c-section-h2 c-section-h2--small-margin">
 					<span class="c-section-h2--orange">консультация</span></h2>
 
@@ -14,13 +14,17 @@
 						<input class="uk-input" id="form-s-text" type="text" placeholder="Ваше имя">
 					</div>
 				</div>
+
 				<div class="uk-margin-top">
 					<label class="uk-form-label" for="form-s-email">Ваш e-mail</label>
 					<div class="uk-inline">
 						<span class="uk-form-icon" uk-icon="icon: mail"></span>
-						<input v-validate="'required|email'" :class="{'input': true, 'uk-form-danger': errors.has('email') }" name="email" class="uk-input" id="form-s-email" type="email" placeholder="name@domain.com">
+						<input v-validate="'required|email'" :class="{'input': true, 'uk-form-danger': errors.has('email') }" name="email" class="uk-input" type="email" placeholder="name@domain.com">
+
 					</div>
+					<div v-show="errors.has('email')" class="text-danger">{{ errors.first('email') }}</div>
 				</div>
+
 				<div class="uk-margin-top">
 					<label class="uk-form-label" for="form-s-tel">Ваш телефон</label>
 					<div class="uk-inline">
@@ -29,7 +33,7 @@
 					</div>
 				</div>
 
-				<button class="uk-button uk-button-primary uk-margin-top">Получить консультацию</button>
+				<button class="uk-button uk-button-primary uk-margin-top" :disabled="errors.any()">Получить консультацию</button>
 				<!--		<input class="c-section-input" type="text" placeholder="Ваше имя">
 						<input class="c-section-input" type="email" placeholder="Ваш e-mail">
 						<input class="c-section-input" type="tel" placeholder="Ваш телефон">
@@ -42,7 +46,18 @@
 
 <script>
 	export default {
-		name: 'ModalKonsultaciya'
+		name: 'ModalKonsultaciya',
+		methods: {
+			submitForm () {
+				this.$validator.validateAll().then(res => {
+					if (res) {
+						alert('Form successfully submitted!')
+					} else {
+						alert('Please correct all error!')
+					}
+				})
+			}
+		}
 	}
 </script>
 
